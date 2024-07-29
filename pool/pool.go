3d43072adda21341cd2002c77ff68160803cedb9
ckikoo <lj5683w@gmail.com>
2024-07-30 00:23:43 +0800

    define new pool

--- pool/pool.go
+++ pool/pool.go
@@ -1,6 +1,7 @@
 package pool
 
 import (
+	"errors"
 	"net"
 	"sync"
 	"time"
@@ -34,4 +35,66 @@ type TcpPool struct {
 	openingConnNum int            // 使用的链接数量
 	idleList       chan *IdleConn // 空闲
 	mu             sync.Mutex     // 保证conn
+}
+
+func NewTcpPool(addr string, poolConfig PoolConfig) (*TcpPool, error) {
+	if addr == "" {
+		return nil, errors.New("出错了")
+	}
+
+	// config校验
+	// 校验工厂
+	if poolConfig.Factory == nil {
+		return nil, errors.New("factory is nil")
+	}
+	const defaultMaxNum = 100
+	const defaultMinConNum = 5
+
+	//修正最大
+	if poolConfig.MaxCountNum == 0 {
+		poolConfig.MaxCountNum = defaultMaxNum
+	}
+	//修正最小
+	if poolConfig.MinConNum == 0 {
+		poolConfig.MinConNum = defaultMinConNum
+	} else if poolConfig.MinConNum > poolConfig.MaxCountNum {
+		poolConfig.MinConNum = defaultMinConNum
+	}
+
+	// 修正空闲
+	if poolConfig.MaxIdleNum == 0 {
+		poolConfig.MaxIdleNum = poolConfig.MinConNum
+	} else if poolConfig.MaxIdleNum > poolConfig.MaxIdleNum-poolConfig.MinConNum {
+		poolConfig.MaxIdleNum = poolConfig.MaxIdleNum - poolConfig.MinConNum
+	}
+
+	pool := TcpPool{config: &poolConfig, idleList: make(chan *IdleConn, poolConfig.MaxIdleNum)}
+
+	// 初始化链接 --最小
+
+	for i := 0; i < pool.config.MinConNum; i++ {
+		conn, err := pool.config.Factory.Factory()
+		if err != nil {
+			// 连接处初始化失败
+			pool.Release() //初始化失败 , 但有可能部分链接成功
+			return nil, err
+		}
+
+		pool.idleList <- &IdleConn{conn: conn, putTime: time.Now()}
+
+	}
+	return &pool, nil
+}
+func (pool *TcpPool) Get() (any, error) {
+	return nil, nil
+}
+func (pool *TcpPool) Put(any) error {
+	return nil
+}
+func (pool *TcpPool) Release() error {
+	return nil
+
+}
+func (pool *TcpPool) Len() int {
+	return 0
 }
